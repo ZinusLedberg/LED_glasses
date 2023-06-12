33be07394afe1208760db9b3869563e1d3c643dd
Linus Svedberg <linussx@hotmail.com>
2023-06-12 17:56:51 +0200

    Added simple header to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,36 @@
 #include <Arduino.h>
-#include<LNEffects.h>
-// the new branch
+#include <LNEffects.h>
+
+//----------------------------------------------------
+// Title: LED program for esp32
+//
+// Authors: Linus Svedberg, Noah Otte
+// Created: 2023-06-08
+// Latest revision: YYYY-MM-DD
+//
+// Description: This program is made to switch between
+//    different modes and effects for an LED-strip
+//    connected to an ESP32 module.
+//    Remember to change each INIT in the setup 
+//    to match your speciffic LED-strip and 
+//    ESP32 module.
+// 
+//----------------------------------------------------
+
 // put function declarations here:
 int myFunction(int, int);
 
+
 void setup() {
   // put your setup code here, to run once:
+
+  // INIT LED
+
+  // INIT MCU
+
   int result = myFunction(10, 3);
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -19,8 +42,9 @@ void loop() {
   // BIG ASS SWITCH
   // reads one of the modes, specified in separate files
 
-  // EXCECUTES THE SELECTED MODE
+  // EXCECUTES/DISPLAY THE SELECTED MODE
 }
+
 
 // put function definitions here:
 int myFunction(int x, int y) {
